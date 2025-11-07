e1c24adfd5abbe7da7a115914fb44b32e5d7e6b4
AiraVen <shaneaira.venenoso@gmail.com>
2025-11-07 12:51:25 +0800

    first commit

--- checkout.php
+++ checkout.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . 'db.php';
+require_once __DIR__ . '/api/db.php';
 require_once __DIR__ . '/api/Session.php';
 
 if (session_status() === PHP_SESSION_NONE) { session_start(); }
